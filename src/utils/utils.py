f1831153e7ddd728d27c0b700a2de31f6957e057
Nikita Stepanov <stepanov2000nikita@yandex.ru>
2021-11-18 12:42:02 +0300

    add seed_all func

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,2 +1,14 @@
+import random
+
+import torch
+
+
+def seed_all(seed=3407):
+    _ = torch.manual_seed(seed)
+    random.seed(seed)
+    torch.use_deterministic_algorithms(True)
+    return seed
+
+
 def count_parameters(model):
     return sum([p.numel() for p in model.parameters()])
