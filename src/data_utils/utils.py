b4f22462de9529a28bfb12bc53136614bf10623e
Nikita Stepanov <stepanov2000nikita@yandex.ru>
2021-11-18 22:11:24 +0300

    add functionality to get_dataloaders

--- src/data_utils/utils.py
+++ src/data_utils/utils.py
@@ -6,17 +6,20 @@ from .sampler import get_sampler
 
 def get_dataloaders(train_ds, val_ds, batch_size,
                     num_workers=2, pin_memory=True):
-    train_sampler = get_sampler(train_ds.csv['label'].values)
+    train_loader, val_loader = None, None
 
-    train_loader = torch.utils.data.DataLoader(
-                            train_ds, batch_size=batch_size,
-                            shuffle=False, collate_fn=Collator(),
-                            sampler=train_sampler,
-                            num_workers=num_workers, pin_memory=pin_memory)
+    if train_ds is not None:
+        train_sampler = get_sampler(train_ds.csv['label'].values)
+        train_loader = torch.utils.data.DataLoader(
+                                train_ds, batch_size=batch_size,
+                                shuffle=False, collate_fn=Collator(),
+                                sampler=train_sampler,
+                                num_workers=num_workers, pin_memory=pin_memory)
 
-    val_loader = torch.utils.data.DataLoader(
-                            val_ds, batch_size=batch_size,
-                            shuffle=False, collate_fn=Collator(),
-                            num_workers=num_workers, pin_memory=pin_memory)
+    if val_ds is not None:
+        val_loader = torch.utils.data.DataLoader(
+                                val_ds, batch_size=batch_size,
+                                shuffle=False, collate_fn=Collator(),
+                                num_workers=num_workers, pin_memory=pin_memory)
 
     return train_loader, val_loader
